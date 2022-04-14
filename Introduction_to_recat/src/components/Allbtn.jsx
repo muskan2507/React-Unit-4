7c0f4b011167984210cc808dd053e0c5db5af9f4
Muskan Shaw <princesahil0065@gmail.com>
2022-04-14 23:03:32 +0530

    tic

--- Introduction_to_recat/src/components/Allbtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Allbtn(props){
-    return (
-        <div className="Btn">
-            <button id={props.id}>{props.name}</button>
-        </div>
-    )
-}
-export default Allbtn
